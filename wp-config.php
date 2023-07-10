7c383e3ac4abac49a99f797441731b0223f89709
Kushka Roman <romanovich9102@gmail.com>
2023-07-10 20:22:57 +0300

    test commit

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,7 @@
  *
  * @package WordPress
  */
-
+//TEST COMMIT
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'server' );
