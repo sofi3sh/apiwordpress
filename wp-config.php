2733a8c65eb4ce3827821511f6148c8ac66fed1a
Volodymyr Bakumenko <volodya.bakumenko@gmail.com>
2023-07-10 21:24:20 +0300

    Test commit

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'server' );
+define( 'DB_NAME', 'server' ); // Test comment
 
 /** Database username */
 define( 'DB_USER', 'root' );
